9e75b32129291b3978c6437106c3ce5e6cda7c9d
aguanteboca <perez.cesaar@gmail.com>
2016-08-30 21:27:04 -0300

    Initial commit

---
+++ src/test/scala-2.11/ar/edu/unq/obj3/demo/WorldTest.scala
@@ -0,0 +1,13 @@
+package ar.edu.unq.obj3.demo
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by perez on 25/08/16.
+  */
+class WorldTest extends FlatSpec{
+
+  "La Aritmetica" should "funcianar" in{
+   assert(2==1+1)
+  }
+}
